7d30a684e812343887068253d981b650ef1faf19
nina <ninazugaj25@gmail.com>
2026-04-08 15:51:15 +0200

    Update SelectRunCurrentCommandOptionListener.java

--- src/test/java/edu/kis/powp/jobs2d/events/SelectRunCurrentCommandOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectRunCurrentCommandOptionListener.java
@@ -4,29 +4,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.command.DriverCommand;
-import edu.kis.powp.jobs2d.drivers.RecordingDriver;
+import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.features.CommandsFeature;
-import edu.kis.powp.jobs2d.features.RecordingFeature;
 
 public class SelectRunCurrentCommandOptionListener implements ActionListener {
 
-    public SelectRunCurrentCommandOptionListener() {
+    private DriverManager driverManager;
+
+    public SelectRunCurrentCommandOptionListener(DriverManager driverManager) {
+        this.driverManager = driverManager;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         DriverCommand command = CommandsFeature.getDriverCommandManager().getCurrentCommand();
-        if (command == null) {
-            return;
-        }
-
-        RecordingDriver rec = RecordingFeature.getRecordingDriver();
-        boolean previousState = rec.isRecordingEnabled();
-        rec.setRecordingEnabled(false);
-        try {
-            command.execute(RecordingFeature.getRecordingDriver());
-        } finally {
-            rec.setRecordingEnabled(previousState);
-        }
+        command.execute(driverManager.getCurrentDriver());
     }
 }
